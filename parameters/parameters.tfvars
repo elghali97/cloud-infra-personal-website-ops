55882b61935260e7be812a80a4c1e4c8f170887a
El Ghali Benchekroun <38661414+elghali97@users.noreply.github.com>
2021-08-25 15:06:46 +0200

    Change Papertrail Add-On plan && Change remote backend (#1)
    Co-authored-by: elghali.benchekroun <elghali.benchekroun@domain.loc>

--- parameters/parameters.tfvars
+++ parameters/parameters.tfvars
@@ -2,8 +2,10 @@
 ############# PARAMETERS ############
 #####################################
 
+// !          File currently not used             ! //
+// ! Parameters sets on Terraform Cloud Workspace !//
 
 # General settings
-app_name          = "personnal-website"
+app_name          = "personal-website"
 app_url           = "www.elghali-benchekroun.com"
 heroku_app_region = "eu"
--- remote-state.tf
+++ remote-state.tf
@@ -4,16 +4,12 @@
 
 # We need to define a remote state in S3 bucket in order to save the state
 
-terraform{
-    backend "s3"{}
-    required_providers {
-        aws = {
-            source  = "hashicorp/aws"
-            version = "~> 3.0"
-        }
-        heroku = {
-            source = "heroku/heroku"
-            version = "~> 4.6"
-        }
+terraform {
+  backend "remote" {}
+  required_providers {
+    heroku = {
+      source  = "heroku/heroku"
+      version = "~> 4.6"
     }
+  }
 }
